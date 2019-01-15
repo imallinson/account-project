8b18094d95b4f16ef27f3a11aa38a40e10140575
imallinson <imallinson1001@gmail.com>
2019-01-15 12:50:56 +0000

    Account class Add and get accounts

---
+++ src/main/java/com/qa/Service.java
@@ -0,0 +1,25 @@
+package com.qa;
+
+import java.util.HashMap;
+
+public class Service {
+	private HashMap<Integer, Account> accounts = new HashMap<>();
+	private int counter = 1000000;
+
+	public HashMap<Integer, Account> getAccounts() {
+		return accounts;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+	
+	public void addAccount(String firstName, String lastName) {
+		accounts.put(counter, new Account(firstName, lastName, counter));
+		counter++;
+	}
+	
+	public Account getAccount(int accountNumber) {
+		return accounts.get(accountNumber);
+	}
+}
