97b57511497145be80cc1c3854deabc9a23df4ff
imallinson <imallinson1001@gmail.com>
2019-01-16 11:15:17 +0000

    modified tests

--- src/test/java/com/qa/testing/Tests.java
+++ src/test/java/com/qa/testing/Tests.java
@@ -11,6 +11,9 @@ public class Tests {
 	@Before
 	public void before() {
 		service = new Service();
+		service.addAccount("Ian", "Mallinson");
+		service.addAccount("Bob", "Mallinson");
+		service.addAccount("Ian", "Mallinson");
 	}
 	
 	@Test
@@ -20,7 +23,6 @@ public class Tests {
 	
 	@Test
 	public void getTest() {
-		service.addAccount("Ian", "Mallinson");
 		assertEquals("Ian" , service.getAccount(1).getFirstName());
 		assertEquals("Mallinson" , service.getAccount(1).getLastName());
 		assertEquals(1 , service.getAccount(1).getAccountNumber());
@@ -28,15 +30,11 @@ public class Tests {
 	
 	@Test
 	public void jsonTest() {
-		service.addAccount("Ian", "Mallinson");
 		assertEquals("{\"firstName\":\"Ian\",\"lastName\":\"Mallinson\",\"accountNumber\":1}" , service.getJSON(1));
 	}
 	
 	@Test
 	public void nameSearchTest() {
-		service.addAccount("Ian", "Mallinson");
-		service.addAccount("Bob", "Mallinson");
-		service.addAccount("Ian", "Mallinson");
 		assertEquals(2, service.searchName("Ian"));
 		assertEquals(1, service.searchName("Bob"));
 	}
