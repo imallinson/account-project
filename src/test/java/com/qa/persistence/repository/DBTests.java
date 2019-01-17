327817216362140780d3bc13881446b5e3291295
imallinson <imallinson1001@gmail.com>
2019-01-17 10:36:59 +0000

    DB tests (not yet implemented)

---
+++ src/test/java/com/qa/persistence/repository/DBTests.java
@@ -0,0 +1,37 @@
+package com.qa.persistence.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DBTests {	
+	@Test
+	public void addAccountTest() {
+		fail("not implemented");
+	}
+	
+	@Test
+	public void getAccountsTest() {
+		fail("not implemented");
+	}
+	
+	@Test
+	public void deleteAccountTest() {
+		fail("not implemented");
+	}
+	
+	@Test
+	public void deleteAccountNotExistingTest() {
+		fail("not implemented");
+	}
+	
+	@Test
+	public void updateAccountTest() {
+		fail("not implemented");
+	}
+	
+	@Test
+	public void updateAccountNotExistingTest() {
+		fail("not implemented");
+	}
+}
