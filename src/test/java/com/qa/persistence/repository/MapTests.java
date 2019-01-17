970c4f25a5fa532d7d4797e262ab50f4bf13458a
imallinson <imallinson1001@gmail.com>
2019-01-17 11:21:37 +0000

    fixed map tests

--- src/test/java/com/qa/persistence/repository/MapTests.java
+++ src/test/java/com/qa/persistence/repository/MapTests.java
@@ -11,31 +11,29 @@ import com.qa.persistence.domain.AccountMap;
 
 public class MapTests {
 	AccountMapRepository accounts;
-	Account account1;
-	Account account2;
+	Account testAccount;
 	
 	@Before
 	public void before() {
 		accounts = new AccountMapRepository();
 		AccountMap.getInstance().setAccounts(new HashMap<>());
-		account1 = new Account("Ian", "Mallinson", "1");
-		account2 = new Account("Bob", "Mallinson", "2");
+		testAccount = new Account("Ian", "Mallinson", "1");
 	}
 	
 	@Test
 	public void addAccountTest() {
-		assertEquals("{\"message\": \"account has been sucessfully added\"}", account1);
+		assertEquals("{\"message\": \"account has been sucessfully added\"}", accounts.createAccount(testAccount));
 	}
 	
 	@Test
 	public void getAccountsTest() {
-		accounts.createAccount(account1);
+		accounts.createAccount(testAccount);
 		assertEquals("{\"1\":{\"firstName\":\"Ian\",\"lastName\":\"Mallinson\",\"accountNumber\":\"1\"}}", accounts.getAllAccounts());
 	}
 	
 	@Test
 	public void deleteAccountTest() {
-		accounts.createAccount(account1);
+		accounts.createAccount(testAccount);
 		assertEquals("{\"message\": \"account has been sucessfully deleted\"}", accounts.deleteAccount("1"));
 		assertEquals("{}" ,accounts.getAllAccounts());
 	}
@@ -47,13 +45,14 @@ public class MapTests {
 	
 	@Test
 	public void updateAccountTest() {
-		accounts.createAccount(account1);
-		assertEquals("{\"message\": \"account has been sucessfully updated\"}", accounts.updateAccount(account2));
-		assertEquals("{\"2\":{\"firstName\":\"Bob\",\"lastName\":\"Mallinson\",\"accountNumber\":\"2\"}}", accounts.getAllAccounts());
+		accounts.createAccount(testAccount);
+		testAccount.setFirstName("Bob");
+		assertEquals("{\"message\": \"account has been sucessfully updated\"}", accounts.updateAccount(testAccount));
+		assertEquals("{\"1\":{\"firstName\":\"Bob\",\"lastName\":\"Mallinson\",\"accountNumber\":\"1\"}}", accounts.getAllAccounts());
 	}
 	
 	@Test
 	public void updateAccountNotExistingTest() {
-		assertEquals("{\"message\": \"account does not exist\"}", accounts.updateAccount(account2));
+		assertEquals("{\"message\": \"account does not exist\"}", accounts.updateAccount(testAccount));
 	}
 }
